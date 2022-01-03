36e9900acd45de284dff06794af93579598698cd
jreleaserbot <jreleaser@kordamp.org>
2022-01-03 15:00:52 +0100

    neo4j-migrations-no-arch 42.23

--- neo4j_migrations.java
+++ neo4j_migrations.java
@@ -1,6 +1,10 @@
-//JAVA 11
-///usr/bin/env jbang "$0" "$@" ; exit $?
+//usr/bin/env jbang "$0" "$@" ; exit $?
+//JAVA 8+
 //DEPS eu.michael-simons.neo4j:neo4j-migrations-cli:1.2.0
+// Generated with JReleaser 0.10.0 at 2022-01-03T14:35:21.606+01:00
 
 public class neo4j_migrations {
+    public static void main(String... args) throws Exception {
+        ac.simons.neo4j.migrations.cli.MigrationsCli.main(args);
+    }
 }
