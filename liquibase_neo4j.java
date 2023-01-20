93229d5d4d7eecb442e5208938590c8e162b4ac2
Florent Biville <445792+fbiville@users.noreply.github.com>
2023-01-20 17:19:46 +0300

    Update to latest version of Liquibase (#3)
    * Bump picocli as well

--- liquibase_neo4j.java
+++ liquibase_neo4j.java
@@ -1,9 +1,8 @@
 //usr/bin/env jbang "$0" "$@" ; exit $?
 //JAVA 8+
-//DEPS org.liquibase.ext:liquibase-neo4j:4.9.1
-//DEPS org.liquibase:liquibase-core:4.9.1
-//DEPS info.picocli:picocli:4.6.1
-//DEPS org.neo4j:neo4j-jdbc-bolt:4.0.5
+//DEPS org.liquibase.ext:liquibase-neo4j:4.19.0
+//DEPS org.liquibase:liquibase-core:4.19.0
+//DEPS info.picocli:picocli:4.7.0
 
 public class liquibase_neo4j {
     public static void main(String... args) throws Exception {
