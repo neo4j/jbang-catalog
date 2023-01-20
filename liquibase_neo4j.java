0218c836aee6c26587d86a802dc75e915f794e29
Florent Biville <florent.biville@neo4j.com>
2023-01-20 12:09:32 +0300

    Bump picocli as well

--- liquibase_neo4j.java
+++ liquibase_neo4j.java
@@ -2,7 +2,7 @@
 //JAVA 8+
 //DEPS org.liquibase.ext:liquibase-neo4j:4.19.0
 //DEPS org.liquibase:liquibase-core:4.19.0
-//DEPS info.picocli:picocli:4.6.1
+//DEPS info.picocli:picocli:4.7.0
 
 public class liquibase_neo4j {
     public static void main(String... args) throws Exception {
