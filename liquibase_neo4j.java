24a30d17d4c440c4afd8c254d211977f27ebc91f
Florent Biville <florent.biville@neo4j.com>
2023-11-03 17:05:11 +0100

    Rely on Liquibase CLI artifact, get rid of picocli

--- liquibase_neo4j.java
+++ liquibase_neo4j.java
@@ -1,8 +1,7 @@
 //usr/bin/env jbang "$0" "$@" ; exit $?
 //JAVA 8+
 //DEPS org.liquibase.ext:liquibase-neo4j:4.24.0
-//DEPS org.liquibase:liquibase-core:4.24.0
-//DEPS info.picocli:picocli:4.7.5
+//DEPS org.liquibase:liquibase-cli:4.24.0
 
 public class liquibase_neo4j {
     public static void main(String... args) throws Exception {
