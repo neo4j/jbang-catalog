d766756e7b950de671da038b99669f754aa44bb5
Florent Biville <445792+fbiville@users.noreply.github.com>
2025-01-20 14:10:36 +0100

    Use latest Liquibase release

--- liquibase_neo4j.java
+++ liquibase_neo4j.java
@@ -1,7 +1,7 @@
 //usr/bin/env jbang "$0" "$@" ; exit $?
 //JAVA 8+
-//DEPS org.liquibase.ext:liquibase-neo4j:4.30.0
-//DEPS org.liquibase:liquibase-core:4.30.0
+//DEPS org.liquibase.ext:liquibase-neo4j:4.31.0
+//DEPS org.liquibase:liquibase-core:4.31.0
 //DEPS info.picocli:picocli:4.7.6
 
 public class liquibase_neo4j {
