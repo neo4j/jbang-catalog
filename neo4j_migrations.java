45f4b67ae552818f844273c383b15df2f59e61e7
jreleaserbot <jreleaser@kordamp.org>
2025-02-18 10:06:13 +0000

    Bump neo4j-migrations to 2.16.1.

--- neo4j_migrations.java
+++ neo4j_migrations.java
@@ -1,7 +1,7 @@
 //usr/bin/env jbang "$0" "$@" ; exit $?
 //JAVA 17+
-//DEPS eu.michael-simons.neo4j:neo4j-migrations-cli:2.16.0
-// Generated with JReleaser 1.5.1 at 2025-02-11T09:01:51.995301826Z
+//DEPS eu.michael-simons.neo4j:neo4j-migrations-cli:2.16.1
+// Generated with JReleaser 1.16.0 at 2025-02-18T10:05:59.92238748Z
 
 public class neo4j_migrations {
     public static void main(String... args) throws Exception {
